9324c96182e7da3a72ac1b711143dc6620ce542e
Kamil Mirosz <kmirosz6@gmail.com>
2019-09-11 16:47:49 +0200

    mini-cart

---
+++ resources/assets/scripts/components/remove-from-cart.js
@@ -0,0 +1,57 @@
+import miniCart from './mini-cart';
+import 'jquery';
+
+
+const CONFIG = {
+    ELEM: '[remove-from-cart]',
+};
+
+const addToCart = {
+    init() {
+        const { ELEM, QAN } = CONFIG;
+
+        this.$elem = document.querySelectorAll(ELEM);
+        this.$qan = document.querySelector(QAN);
+        console.log(this.$elem);
+        this.addEvent();
+        this.$allow = true;
+    },
+
+    addEvent() {
+        this.$elem.forEach((element) => {
+            element.addEventListener('click', (event) => {
+                event.preventDefault();
+
+                if (this.$allow) {
+                    this.$allow = false;
+                    const link = element.getAttribute('href');
+                    console.log(link);
+                    this.loadBasket(element, link);
+                }
+            });
+        });
+    },
+
+    loadBasket(link) {
+        const $this = this;
+
+        if (miniCart.countItems() === 1) {
+            miniCart.hide();
+        }
+
+        $('#mini-card').load(`${link} #mini-card > *`, function () {
+            miniCart.init();
+
+
+            $this.$allow = true;
+        });
+
+    },
+
+    changeQuantity() {
+        this.$qan.innerText = Number(this.$qan.innerText) + 1;
+    },
+
+};
+
+export default addToCart;
